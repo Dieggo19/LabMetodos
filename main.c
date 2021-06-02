2420cf77f2b822f7209ea1ffd37d5c9e3132799f
Danaxar <74688364+Danaxar@users.noreply.github.com>
2021-06-02 11:11:16 -0400

    Creación del código fuente.
    Aquí estará el programa principal en caso de hacer otro dedicado a funciones para trabajar más limpiamente.

---
+++ main.c
@@ -0,0 +1,15 @@
+// IMPORTACIONES
+#include <stdio.h>
+// CONSTANTES
+// ESTRUCTURAS
+// FUNCIONES
+// PRINCIPAL
+int main(){
+	// ENTRADA
+	printf("Ejecucion iniciada");
+	// PROCESAMIENTO
+	
+	// SALIDA
+	printf("Ejecucion finalizada");
+	return 0;
+}
