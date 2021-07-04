91e8183395f82f53d72a59a4eb4312b65ea6f0a2
Daniel Catalán <daniel.catalan@usach.cl>
2021-07-04 17:39:34 -0400

    Actualización final de la entrega formativa

--- principal.c
+++ principal.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include "funciones_1.h"
-// Agrega acá: ' #include "funciones_2.h  '
+#include "funciones_2.h"
 
 
 
-int main(int argc, char const *argv[])
+int main()
 {	// Pedir tipo de entrada
 	printf("\nEjecucion iniciada\n"); printf("Que archivo desea leer?\n");
 	printf("1) Entrada.in\n2) Entrada2.in\n: "); int respuesta;
@@ -43,20 +43,77 @@ int main(int argc, char const *argv[])
 		
 	}else{
 		//			Entrada2.in 			//
-		// DIEGO ACUÑA (BORRAR ESTE COMENTARIO DESPUÉS)
+        int n = 0;
+        int **mapa = leerEntrada(&n);
+        FILE *ArchivoSalida = fopen("Salida2.in", "w");
+        printf("\n");
+        
 
-		// Funcion que lee archivo:
-		// LeerEntrada("nombre_archivo.in", puntero_de_dimension (tipo int))
+        // IMPRIMIR DATOS OBTENIDOS
+        printf("ENTRADA\n");
+        fprintf(ArchivoSalida, "%d\n", n);
+        printf("%d\n", n);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                printf("%d ", mapa[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n\n");
+        
+        //RECORRER FILAS 
+        printf("SALIDA\n");
+        printf("%d\n", n);
+        for (int i = 0; i < n; i++)
+        {
+            int acum = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if(mapa[i][j] != 0){
+                    acum++;
+                }
+            }
+            fprintf(ArchivoSalida, "%d ", acum);
+            printf("%d ", acum);
+        // printf("\n");
+            for (int j = 0; j < n; j++)
+            {
+                if(mapa[i][j] != 0){
+                fprintf(ArchivoSalida, "%d ", mapa[i][j]);
+                printf("%d ", mapa[i][j]);
+                }
+            }
+            fprintf(ArchivoSalida, "\n");
+            printf("\n");
+        }
 
-
-
-		/*
-
-
-			                <CODIGO>
-
-
-		*/
+        //RECORRER COLUMNAS
+        for(int i = 0; i < n; i++)
+        {
+            int contador = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if(mapa[j][i] != 0){
+                    contador++;
+                }
+            }
+            fprintf(ArchivoSalida, "%d ", contador);
+            printf("%d ", contador);
+            for (int j = 0; j < n; j++)
+            {
+                if(mapa[j][i] != 0){
+                fprintf(ArchivoSalida, "%d ", mapa[j][i]);
+                printf("%d ", mapa[j][i]);
+                }
+            }
+            fprintf(ArchivoSalida, "\n");
+            printf("\n");
+        }
+        fclose(ArchivoSalida);
+        printf("\nArchivo creado correctamente, revise salida2.in\n\n");
+		
 	}
 
 
@@ -77,9 +134,3 @@ int main(int argc, char const *argv[])
 	printf("Programa finalizado.");
 	return 0;
 }
-
-
-
-
-
-
